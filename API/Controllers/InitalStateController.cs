f7aa35cf4779e4e836497de99b1b7019b32e7c7b
jasmin289 <jsmina30@gmail.com>
2023-09-17 11:08:29 +0300

    api working

---
+++ API/Controllers/InitalStateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndProject.API.Controllers
+{
+    public class InitalStateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
