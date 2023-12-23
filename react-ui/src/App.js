86ba230422a0ca41adfa35f0b1ea4df2b23b80b7
jasmin289 <jsmina30@gmail.com>
2023-12-23 16:28:59 +0200

    working vrsion 23.12.2023

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from '../../../../../node_modules/react-router-dom/index';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<div>Login</div> }/>
+                <Route path="/login" element={<div>Login</div>} />
+            
+                <Route path="/" element={<div>Home</div>}></Route>
+            </Routes>
+        </BrowserRouter>
+      
   );
 }
 
